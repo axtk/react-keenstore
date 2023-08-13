c2e385f6c91af2528874234ec0b5a4ecedb3750c
axtk <axtkx@inbox.ru>
2023-08-13 18:15:46 +0300

    allow filtering store updates based on nextState and prevState

--- index.ts
+++ index.ts
@@ -4,21 +4,26 @@ import type {Store} from 'keenstore';
 export * from 'keenstore';
 
 export type SetStoreState<T> = Store<T>['setState'];
+export type IsStoreResponsive<T> = (nextState: T, prevState: T) => boolean;
 
-export function useStore<T>(store: Store<T>, subscribes = true): [T, SetStoreState<T>] {
+export function useStore<T>(
+    store: Store<T>,
+    responsive: boolean | IsStoreResponsive<T> = true,
+): [T, SetStoreState<T>] {
     let [, setRevision] = useState(-1);
 
     let state = store.getState();
     let setState = useMemo(() => store.setState.bind(store), [store]);
 
     useEffect(() => {
-        if (!subscribes)
+        if (!responsive)
             return;
 
-        return store.onUpdate(() => {
-            setRevision(Math.random());
+        return store.onUpdate((nextState, prevState) => {
+            if (typeof responsive !== 'function' || responsive(nextState, prevState))
+                setRevision(Math.random());
         });
-    }, [store, subscribes]);
+    }, [store, responsive]);
 
     return [state, setState];
 }
